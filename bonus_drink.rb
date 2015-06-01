d1a3114f88a617b9bc7f28a6aa9e70f0b898e11c
Makoto Nakayama <makoto0306@gmail.com>
2015-06-01 16:42:05 +0900

    解答を追加

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,15 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    changed = amount / 3
+
+    rest = amount % 3
+    return rest if changed == 0
+
+    empty = amount - rest
+
+    empty + total_count_for(rest + changed)
   end
 end
+
+puts BonusDrink.total_count_for(100)
+# 答えは149本でした
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+
+  def total_count_for(num)
+    BonusDrink.total_count_for(num)
+  end
+
+  specify { expect(total_count_for(0)).to eq 0 }
+  specify { expect(total_count_for(1)).to eq 1 }
+  specify { expect(total_count_for(3)).to eq 4 }
+  specify { expect(total_count_for(10)).to eq 14 }
+  specify { expect(total_count_for(11)).to eq 16 }
 end
